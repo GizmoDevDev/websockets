709c68a9654cb836211e37df33cf983a942417f4
Gizmo <gizmo.dev.dev@gmail.com>
2023-04-04 22:44:39 +0300

    Простая реализация

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,33 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+type Message = {
+  name: string,
+  text: string,
+  isOur: boolean,
+} | {
+  name: string,
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [ws, setWS] = useState<WebSocket | null>(null)
+  const [input, setInput] = useState('')
+  const [name, setName] = useState('Biba')
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handleConnectButtonClick = () => {
+    if (ws) return;
+    const webSocket = new WebSocket('ws://localhost:4000')
+    webSocket.onopen = () => {
+      console.log('ws open')
+      setWS(webSocket)
+      webSocket.send(JSON.stringify({type: 'Connect', payload: { name }}))
+    }
+
+    webSocket.onclose = () => {
+      console.log('ws close')
+      setWS(null)
+    }
+
+    webSocket.onmessage = (event) => {
+      const evData = JSON.parse(event.data)
+      switch(evData.type) {
+        case 'Connect':
+          setMessages((prev) => [
+            ...prev,
+            { name: evData.payload.name }
+          ])
+          break;
+        case 'Message':
+          setMessages((prev) => [
+            ...prev,
+            { name: evData.payload.name, text: evData.payload.message, isOur: false }
+          ])
+          break;
+        default:
+          console.log('UNKNOWN EVENT')
+      }
+    }
+  }
+
+  const handleCloseButtonClick = () => {
+    if (!ws) return;
+    ws.close();
+  }
+
+  const handleSendMessage = () => {
+    if (!ws) return;
+    ws.send(JSON.stringify({type: 'Message', payload: {name, message: input}}));
+    setMessages((prev) => [...prev, { name, text: input, isOur: true }]);
+    setInput('');
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Name: <input value={name} onChange={({target}) => setName(target.value)} /><br />
+      {messages.map((message) => (<p>
+          { 'isOur' in message
+            ? <>{message.isOur ? (<b>{message.name}</b>) : message.name}: {message.text}</>
+            : <i>{message.name} was connected</i>}
+      </p>))}
+
+      <input value={input} onChange={({target}) => setInput(target.value)} />
+      <button disabled={!ws} onClick={handleSendMessage}>Send</button>
+        <br />
+      <button disabled={Boolean(ws)} onClick={handleConnectButtonClick}>Connect</button>
+      <button disabled={!ws} onClick={handleCloseButtonClick}>Disconnect</button>
     </div>
   )
 }
